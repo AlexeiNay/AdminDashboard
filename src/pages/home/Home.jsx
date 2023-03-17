2fea5e5228375f520d53c2f851f4929f698e948d
Alexei Sánchez <alexeisanchez66@gmail.com>
2023-03-17 13:19:14 -0700

    Chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
+import Chart from '../../components/chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './home.css'
+import {userdata} from '../../dummyData'
 
 export default function Home() {
   return (
     <div className='home'>
         <FeaturedInfo/>
+        <Chart
+        title= "User Analytics"
+        data={userdata}
+        grid
+        dataKey='Active User'
+        />
     </div>
   )
 }
