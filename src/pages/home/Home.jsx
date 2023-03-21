30863d5666b5e329173b7ce212e11de63f9ffe62
Alexei Sánchez <alexeisanchez66@gmail.com>
2023-03-21 13:10:45 -0700

    Widgets homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,24 @@
-import Chart from '../../components/chart/Chart'
-import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
-import './home.css'
-import {userdata} from '../../dummyData'
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import "./home.css";
+import { userdata } from "../../dummyData";
+import WidgetSm from "../../components/widgetsSm/WidgetSm";
+import WidgetLg from "../../components/widgetsLg/WidgetLg";
 
 export default function Home() {
   return (
-    <div className='home'>
-        <FeaturedInfo/>
-        <Chart
-        title= "User Analytics"
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        title="User Analytics"
         data={userdata}
         grid
-        dataKey='Active User'
-        />
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
     </div>
-  )
+  );
 }
